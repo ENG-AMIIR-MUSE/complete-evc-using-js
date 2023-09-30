7682a8d498fd1f8c900d83dd1a6f049b4f6562f7
AmirMuse <amirlearn08@gmail.com>
2023-09-29 19:00:37 -0700

    finshed the Balance

--- main.js
+++ main.js
@@ -6,11 +6,19 @@ let number  = 612353406;
 let userPin  = 1122;
 
 
+function getBalance(){
+    return balance;
+}
+
+
 
 let userInput   = Number(prompt("-EVCPLUS \n Fadlan Geli Pinkaaga(Enter Your Pin ) :"))
 if(userInput ==  userPin){
-    alert("1 . Haraagaaga Itus \n2 .  Kaarka Ku hadalka \n3 . Bixi Biilka\n4 . U wareeji EVCPLus\n5 . Warbixin kooban \n6 . Salaam Bank\n7 . Maaraynta\n8 . Taaj \n9 . Kabax")
-
+    let userSelection  = Number(prompt("1 . Haraagaaga Itus \n2 .  Kaarka Ku hadalka \n3 . Bixi Biilka\n4 . U wareeji EVCPLus\n5 . Warbixin kooban \n6 . Salaam Bank\n7 . Maaraynta\n8 . Taaj \n9 . Kabax"))
+    switch(userSelection){
+        case  1:
+          alert("EVCPlus : \nHaraagaaga Waa "+ getBalance())
+    }
 }else{
     alert("EVCPLUS \n Pinkaaga Waa Qalad")
 }
