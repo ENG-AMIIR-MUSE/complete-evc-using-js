2d1a3c84bf3cbbe14845afdf75caae97bd304fc9
AmirMuse <amirlearn08@gmail.com>
2023-09-29 18:50:04 -0700

    initial commit evcplus

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+
+let balance  = 0;
+let number  = 612353406;
+let userPin  = 1122;
+
+
+
+let userInput   = Number(prompt("-EVCPLUS \n Fadlan Geli Pinkaaga(Enter Your Pin ) :"))
+if(userInput ==  userPin){
+    alert("1 . Haraagaaga Itus \n2 .  Kaarka Ku hadalka \n3 . Bixi Biilka\n4 . U wareeji EVCPLus\n5 . Warbixin kooban \n6 . Salaam Bank\n7 . Maaraynta\n8 . Taaj \n9 . Kabax")
+
+}else{
+    alert("EVCPLUS \n Pinkaaga Waa Qalad")
+}
+
+console.log(userInput)
